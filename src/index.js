79b8711513e19f6cea2a373778485ada5a457f41
Calvin Brown <calvin@kairuconsulting.com>
2022-07-20 19:41:59 -0400

    comment added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// this is a comment typed directly into GitHub
 var element = React.createElement('h1', { className: 'greeting' }, 'Hello, Black Men and Women Coding!');
 ReactDOM.render(element, document.getElementById('root'));
 
