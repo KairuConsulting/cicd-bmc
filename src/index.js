71a6f4cb89282dde17b861c64c74b94aebfa6db8
Calvin Brown <calvin@kairuconsulting.com>
2022-07-20 19:42:33 -0400

    label updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // this is a comment typed directly into GitHub
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, Black Men and Women Coding!');
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, Black Men and Women Coding! - Updated for the demo');
 ReactDOM.render(element, document.getElementById('root'));
 
 reportWebVitals();
