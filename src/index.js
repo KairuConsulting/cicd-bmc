8e0a447c225cde1ed2d74aa8c2a734886cd90a37
Calvin Brown <calvin@kairuconsulting.com>
2022-07-20 19:46:39 -0400

    index.js updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, Black Men and Women Coding!');
+// this is a comment that we're adding to the code to make a change
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, Black Men and Women Coding! -- this is an updated version of our site');
 ReactDOM.render(element, document.getElementById('root'));
 
 reportWebVitals();
+// this is a test
